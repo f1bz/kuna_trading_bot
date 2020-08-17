6faf595ed90cada155c1b9824ae4cd00e9005b09
f1bz <fibbyua@gmail.com>
2020-08-17 22:56:13 +0300

    Refactor

--- src/main/java/com/tradingbot/kuna/service/indicators/IndicatorFireService.java
+++ src/main/java/com/tradingbot/kuna/service/indicators/IndicatorFireService.java
@@ -13,7 +13,6 @@ public class IndicatorFireService {
     public boolean isReadyToFire(MarketRate marketRate, Indicator indicator) {
         BigDecimal marketValue = marketRate.getBuy();
         BigDecimal signalValue = indicator.getValue();
-
         if (indicator.getIndicatorType().equals(IndicatorType.BY_VALUE_ABOVE)) {
             return marketValue.compareTo(signalValue) >= 0;
         } else if (indicator.getIndicatorType().equals(IndicatorType.BY_VALUE_BELOW)) {
